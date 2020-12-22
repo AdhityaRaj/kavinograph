15a386bdd4675bb3728028ed53dd65ef5b816e82
Adhitya-Raj <Adhitya-Raj@github.com>
2020-12-22 09:40:01 +0530

    renamed about to home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AboutComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
