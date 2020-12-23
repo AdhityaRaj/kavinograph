fa1e81b3c1d28e342cfc5370e0b737a0caf3a585
Adhitya-Raj <Adhitya-Raj@github.com>
2020-12-23 11:26:49 +0530

    pre prod build with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: '**', component: HomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, { 
+    relativeLinkResolution: 'legacy',
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled',
+    onSameUrlNavigation: 'ignore',
+    
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
