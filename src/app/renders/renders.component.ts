7d12b4b3240c478056f74996f90a1a5215a06ce3
Adhitya-Raj <Adhitya-Raj@github.com>
2021-01-08 21:11:51 +0530

    assest added for 3d and all assests optimised

--- src/app/renders/renders.component.ts
+++ src/app/renders/renders.component.ts
@@ -10,6 +10,18 @@ export class RendersComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.createSrcMap();
   }
 
+  imgSrc: Array<Array<String>> = [];
+  
+  createSrcMap() {
+    for (let index = 0; index < 3; index++) {
+      let array: Array<String> = [];
+      for (let no = 1; no<10; no++){
+        array.push('assets/mywork/3d/3d_'+(no+index*10)+'.jpg');
+      }
+      this.imgSrc.push(array);
+    }
+  }
 }
